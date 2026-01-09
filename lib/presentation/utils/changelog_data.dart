da793a3e536db3e25b89a6c7ce3aff5616b3f039
EmersonComar <emerson.comar@ixcsoft.com.br>
2026-01-09 10:15:11 -0300

    fix: ajustado mensagem changelog

--- lib/presentation/utils/changelog_data.dart
+++ lib/presentation/utils/changelog_data.dart
@@ -12,10 +12,9 @@ const List<BackendVersion> changelogData = [
   BackendVersion(
     version: '1.3.5',
     changes: [
-      'Adicionado este campo de changelog para melhor comunicação referente às atualizações do DocFlow.',
-      """Adicionado suporte para snippets nas anotações. Tais snippets permitem que seja possível criar trechos de anotações que serão inseridos no momento
-      de copiar uma anotação. Para utilizar o snippet, basta digitar o nome do snippet entre chaves, por exemplo {{snippet_name}}. Um exemplo:
-      tcpdump -n -i {{interface}} -host {{host}} -p {{port}}""",
+      'Implementação do visualizador de Changelog para acompanhar as novidades e atualizações do DocFlow.',
+      """Adicionado suporte a Snippets nas anotações. Agora você pode definir variáveis dinâmicas utilizando chaves duplas, facilitando a reutilização de comandos.
+      Exemplo: tcpdump -n -i {{interface}} host {{host}} and port {{port}}""",
     ],
   ),
 ];
